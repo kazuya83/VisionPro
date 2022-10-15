de1d260a189a70e3343eb81d47dc022ffed57878
Kazuya Ishikawa <77392836+kazuya83@users.noreply.github.com>
2022-10-16 02:06:42 +0900

    Update corporate_list_repository.py

--- Admin/app/Repositories/corporate_list_repository.py
+++ Admin/app/Repositories/corporate_list_repository.py
@@ -4,6 +4,7 @@ class CorporateListRepository:
     def get_corporate_list() -> dict:
         result = DB.execute_select_sql('''
     select
+        C.no,
         C.corporate_id,
         C.corporate_unique_name,
         C.corporate_name,
@@ -11,6 +12,7 @@ class CorporateListRepository:
     FROM
         T_Corporate C
         LEFT OUTER JOIN T_Corporate_DB DB ON C.corporate_id = DB.corporate_id
+    ORDER BY C.no
     '''
     ,'domain')
         return result
